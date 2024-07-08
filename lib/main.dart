3dedfada05e1a21429dd16a1c9fcdc75758bb91a
Graham Williams <graham.williams@togaware.com>
2024-07-09 09:07:23 +1000

    Cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 /// A template app to begin a Solid Pod project.
 //
-// Time-stamp: <Monday 2024-07-08 19:01:58 +1000 Graham Williams>
+// Time-stamp: <Tuesday 2024-07-09 09:07:17 +1000 Graham Williams>
 //
 /// Copyright (C) 2024, Software Innovation Institute, ANU.
 ///
@@ -35,12 +35,17 @@ import 'package:keypod/utils/is_desktop.dart';
 
 void main() async {
   // This is the main entry point for the app. The [async] is required because
-  // we asynchronously [await] some activity below.
-
-  // Suport placing the new window on top of all other windows on starting up
-  // the desktop app.
+  // we asynchronously [await] the window manager below. Often, `main()` will
+  // simply include just [runApp].
 
   if (isDesktop(PlatformWrapper())) {
+    // Suport [windowManager] options for the desktop. We do this here before
+    // running the app. If there is no [windowManager] options we probably don't
+    // need this whole section.
+
+    // Enusre things are set up properly since we haven't yet initialised the
+    // app with [runApp].
+
     WidgetsFlutterBinding.ensureInitialized();
 
     await windowManager.ensureInitialized();
@@ -49,9 +54,10 @@ void main() async {
       // We can set various desktop window options here.
 
       // Setting [alwaysOnTop] here will ensure the app starts on top of other
-      // apps on the desktop so that it is visible (otherwise it is often lost
-      // below other windows on startup which can be a little disconcerting). We
-      // later turn it off as we don't want to force it always on top.
+      // apps on the desktop so that it is visible (otherwise, Ubuuntu with
+      // GNOME it is often lost below other windows on startup which can be a
+      // little disconcerting). We later turn it off as we don't want to force
+      // it always on top.
 
       alwaysOnTop: true,
 
@@ -60,7 +66,7 @@ void main() async {
       title: 'KeyPod - Private Solid Pod for Storing Key-Value Pairs',
     );
 
-    // Once the window manager is ready wet recofigure it a little.
+    // Once the window manager is ready we recofigure it a little.
 
     await windowManager.waitUntilReadyToShow(windowOptions, () async {
       await windowManager.show();
@@ -74,10 +80,16 @@ void main() async {
   runApp(const KeyPod());
 }
 
+// The main widget could be in a separate file, but handy having it in main and
+// the file is not too large. The widget essentially orchestrates the building
+// of other widgets. Generically we set up to build a `Home()` widget containing
+// the App. For SolidPod we wrap the `Home()` widget within the `SolidLogin()`
+// widget so we start with a login screen, though this is optional.
+
 class KeyPod extends StatelessWidget {
   const KeyPod({super.key});
 
-  // This widget is the root of our application.
+  // This StatelessWidget is the root of our application.
 
   @override
   Widget build(BuildContext context) {
@@ -85,23 +97,22 @@ class KeyPod extends StatelessWidget {
       title: 'Solid Key Pod',
       home: SelectionArea(
         // Wrap the whole app inside a SelectionArea to ensure we get selectable
-        // text where it can be, as a default.
+        // text, for text that can be selected, as a default.
 
         child: SolidLogin(
           // Wrap the actual home widget within a [SolidLogin].
 
           // If the app has functionality that does not require access to Pod
           // data then [required] can be `false`. If the user connects to their
-          // Pod then we can ensure their session information will be saved. If
-          // we aim to save the data to the Pod or view data from the Pod, then
-          // if the user did not log in during startup we can call
-          // [SolidLoginPopup] to establish the connection at that time. The
-          // login token and the security key are (optionally) cached so that
-          // the login information is not required every time.
-
-          // For demonstration purposes we do not require a login through the
-          // SolidLogin. Our app here will request a login immediately anyhow
-          // since it has no functionality without data from the SolidPod.
+          // Pod then their session information will be saved to save having to
+          // log in everytime. The login token and the security key are (optionally)
+          // cached so that the login information is not required every time.
+          //
+          // In this demo app we allow the CONTINUE button so as to demonstrate
+          // the use of [SolidLoginPopup] during the app session. If we want to
+          // save the data to the Pod or view data from the Pod, then if the
+          // user did not log in during startup we can call [SolidLoginPopup] to
+          // establish the connection at that time.
 
           required: false,
 
